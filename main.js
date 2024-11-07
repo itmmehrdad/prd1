efd4ee41e35e82920590380c429aa99dc3631e1c
MehrdadAzadi <mehrdadazadi@MehrdadAzadis-MacBook-Air.local>
2024-11-07 13:55:26 +0330

    hgghhg

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("test");
+const express = require("express");
+const app = express();
+const port = 5500;
+
+app.get("/sabtenam", (req, res) => {
+  res.send("samt nam ba movafaghet anjam shod");
+});
+
+app.get("/test", (req, res) => {
+  res.send("in yek test ast");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
